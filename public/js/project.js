d61ce8f8728969aa48da27cda443c6722aa8346a
AndresRenteria <andres.renteria@hotmail.com>
2016-04-26 02:28:42 -0500

    la primera parte prueba

--- public/js/project.js
+++ public/js/project.js
@@ -1,13 +1,23 @@
-function weightC(n){
+function weightP(n){
   var rating = parseFloat($('#pRating' + n).val());
   var weight = parseFloat($('#pWheight' + n).val());
-  return weight/5*rating
+  var codigo = '<p>'+weight/5*rating+'</p>';
+  $('#pValue'+n).html(codigo);
+
 }
-function tWeightC(n){
+function tWeightC(n,x){
   var total = 0;
   for (var i = 1; i < n + 1; i++) {
     total = total + parseFloat($('#pRating' + i).val());
     total = total + parseFloat($('#pWheight' + i).val());
   }
-  return total
+  var codigo = '<p>'+total+'</p>';
+  $('#tpValue'+x).html(codigo);
+}
+
+function showWeight(){
+  for (var i = 1; i < n + 1; i++) {
+    weightP(i);
+  }
+  tWeightC(4,1)
 }
