ef694aa349726cba7353c2bce529132c88cab6bb
AndresRenteria <andres.renteria@hotmail.com>
2016-04-26 13:06:16 -0500

    errores de formulas

--- public/js/project.js
+++ public/js/project.js
@@ -22,7 +22,7 @@ function weightS(n){
   var rating = parseFloat($('#sRating' + n).val());
   var weight = parseFloat($('#sWheight' + n).val());
   var codigo = '<p>'+weight/5*rating+'</p>';
-  $('#pValue'+n).html(codigo);
+  $('#sValue'+n).html(codigo);
 
 }
 
@@ -42,6 +42,30 @@ function tWeightS(n){
   $("#tsValue1").html(codigo);
 }
 
+function weightT(n){
+  var rating = parseFloat($('#tRating' + n).val());
+  var weight = parseFloat($('#tWheight' + n).val());
+  var codigo = '<p>'+weight/5*rating+'</p>';
+  $('#tValue'+n).html(codigo);
+
+}
+
+function tWeightT(n){
+  var total = 0;
+  var total1 = 0;
+  for (var i = 1; i < n + 1; i++) {
+
+    total = total + parseFloat($('#tValue' + i).val());
+    total1 = total1 + parseFloat($('#tWheight' + i).val());
+
+  }
+  var codigo = '<p>'+total+'</p>';
+  var codigo1 = '<p>'+total1+'</p>';
+
+  $("#ttWheight1").html(codigo1);
+  $("#ttValue1").html(codigo);
+}
+
 function showWeight(){
   for (var i = 1; i <= 4; i++) {
     weightP(i);
@@ -53,8 +77,8 @@ function showWeight(){
   }
   tWeightS(10)
 
-  //for (var i = 1; i <= 8; i++) {
-  //  weightT(i);
-  //}
-  //tWeightT(8)
+  for (var i = 1; i <= 8; i++) {
+    weightT(i);
+  }
+  tWeightT(8)
 }
