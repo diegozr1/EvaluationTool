be9e72f82550ee20c673aef8ff0b785146f7f7d2
Diego Zamora Rodriguez <diego.zr@live.com.mx>
2016-04-26 23:35:00 -0500

    Guardar proyectos

---
+++ public/js/projects.js
@@ -0,0 +1,31 @@
+angular.module('todoApp', [])
+    .controller('TodoListController', function($http) {
+        var todoList = this;
+
+        $http.get("https://api.myjson.com/bins/527de")
+            .then(function(response) {
+                todoList.todos = response.data;
+            });
+        
+        todoList.addTodo = function() {
+            todoList.todos.push({text:todoList.todoText, done:false});
+            todoList.todoText = '';
+        };
+
+        todoList.remaining = function() {
+            var count = 0;
+            angular.forEach(todoList.todos, function(todo) {
+                count += todo.done ? 0 : 1;
+            });
+            return count;
+        };
+
+        todoList.archive = function() {
+            var oldTodos = todoList.todos;
+            todoList.todos = [];
+            angular.forEach(oldTodos, function(todo) {
+                if (!todo.done) todoList.todos.push(todo);
+            });
+        };
+    });
+
