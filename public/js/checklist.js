f08631d7d9c9a75b1a4d8b2300cf313bf812a945
Fernando Chavez <fertux200795@hotmail.com>
2016-05-02 04:33:37 -0500

    Actualizando

--- public/js/checklist.js
+++ public/js/checklist.js
@@ -147,7 +147,7 @@ function printpdf(){
   var contenedorpregunta = "&preguntas[]=";
   var contendorfinal;
   for(i=1;i<20;i++){
-    contendorfinal += contenedorrespuesta+ $("#txtRespuesta"+i).val()+contenedorpregunta+$("#txtPregunta"+i).textContent;
+    contendorfinal += contenedorrespuesta+ $("#txtRespuesta"+i).val()+contenedorpregunta+$("#txtPregunta"+i).text();
   }
   url +=contendorfinal;
 
