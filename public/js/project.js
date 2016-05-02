e2868bb2a1df7636b2410deeb765a1494b00d63c
Fernando Chavez <fertux200795@hotmail.com>
2016-05-02 16:26:28 -0500

    Final

--- public/js/project.js
+++ public/js/project.js
@@ -185,13 +185,13 @@ function printpdf(){
   var contenedorfinal;
   contenedorfinal+=pesopartea+pesoparteb+pesopartec;
   for(i=1;i<5;i++){
-    contenedorfinal += ratinga+$("#pRating:selected"+i).text()+weightinga+parseFloat($("#pWheight"+i).val())+valuea+parseFloat($("#pValue"+i).text());
+    contenedorfinal += ratinga+$("#pRating"+i).text().replace(/ /g,'');+weightinga+parseFloat($("#pWheight"+i).val())+valuea+parseFloat($("#pValue"+i).text());
   }
   for(i=1;i<11;i++){
-    contenedorfinal += ratingb+$("#sRating"+i).text()+weightingb+parseFloat($("#sWheight"+i).val())+valueb+parseFloat($("#sValue"+i).text());
+    contenedorfinal += ratingb+$.trim("#sRating"+i).text())+weightingb+parseFloat($("#sWheight"+i).val())+valueb+parseFloat($("#sValue"+i).text());
   }
   for(i=1;i<9;i++){
-    contenedorfinal += ratingc+$("#tRating"+i).val()+weightingc+parseFloat($("#tWheight"+i).val())+valuec+parseFloat($("#tValue"+i).text());
+    contenedorfinal += ratingc+$("#tRating"+i).text()+weightingc+parseFloat($("#tWheight"+i).val())+valuec+parseFloat($("#tValue"+i).text());
   }
   url +=contenedorfinal+"&approved="+$("#de").text();
 
