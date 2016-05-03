46cc6bb3055c8b46cd9771fec494d14e91336de1
Diego Zamora Rodriguez <diego.zr@live.com.mx>
2016-05-03 14:38:51 -0500

    a

--- public/js/todoprojects.js
+++ public/js/todoprojects.js
@@ -1,40 +1,33 @@
 angular.module('todoApp', [])
-    .controller('TodoListController', function($http) {
-        var todoList = this;
-        todoList.todos = [];
-        var ref = new Firebase("https://evaluationtool.firebaseio.com/");
+  .controller('TodoListController', function() {
+    var todoList = this;
 
-        ref.on("child_added", function(snapshot, prevChildKey) {
-          var data      = snapshot.val();
-          todoList.todos.push(data);
-        });
+    todoList.todos = [];
 
-        /*$http.get("https://evaluationtool.firebaseio.com/.json")
-            .then(function(response) {
-                todoList.todos = response.data;
-                todoList.count = response.data.count;
-            });  */
+    $http.get("https://evaluationtool.firebaseio.com/.json")
+              .then(function(response) {
+                  todoList.todos = response.data;
+              });
 
-        todoList.addTodo = function() {
-            ref.push().set({text:todoList.todoText, done:false})
-            //todoList.todos.push({text:todoList.todoText, done:false});
-            todoList.todoText = '';
-            location.reload();
-        };
 
-        todoList.remaining = function() {
-            var count = 0;
-            angular.forEach(todoList.todos, function(todo) {
-                count += todo.done ? 0 : 1;
-            });
-            return count;
-        };
+    todoList.addTodo = function() {
+      todoList.todos.push({text:todoList.todoText, done:false});
+      todoList.todoText = '';
+    };
 
-        todoList.archive = function() {
-            var oldTodos = todoList.todos;
-            todoList.todos = [];
-            angular.forEach(oldTodos, function(todo) {
-                if (!todo.done) ref.push().set(todo); //todoList.todos.push(todo);
-            });
-        };
-    });
+    todoList.remaining = function() {
+      var count = 0;
+      angular.forEach(todoList.todos, function(todo) {
+        count += todo.done ? 0 : 1;
+      });
+      return count;
+    };
+
+    todoList.archive = function() {
+      var oldTodos = todoList.todos;
+      todoList.todos = [];
+      angular.forEach(oldTodos, function(todo) {
+        if (!todo.done) todoList.todos.push(todo);
+      });
+    };
+  });
